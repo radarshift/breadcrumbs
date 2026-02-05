43d74c9133e79f8a4b30903f014fd86d17ec0781
ari (work) <arielbdanon@gmail.com>
2026-02-05 13:30:35 -0600

    setting up model to sync to github

---
+++ pkg/model/crumb.go
@@ -0,0 +1,28 @@
+package model
+
+import "time"
+
+// Kind represents the type of a Crumb, either a File or a Directory.
+type Kind int
+
+// Idiomatic Enumeration of possible Kind values.
+const (
+	File Kind = iota
+	Directory
+)
+
+type Location struct {
+	Kind Kind
+	Path string
+}
+
+type Crumb struct {
+	Name        string
+	CreatedBy   string
+	Comment     string
+	Link        Location
+	Completed   bool
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	CompletedAt *time.Time
+}
---
+++ pkg/store/store.go
@@ -0,0 +1,1 @@
+// just to get this into github
